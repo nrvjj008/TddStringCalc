bc1f43da74db0dff57981b285fdf5a1894fd8eb5
nrvjj008 <nirav@jeavio.com>
2021-05-07 10:08:16 +0530

    Add upto 2 values

--- src/main/java/com/tddtest/StringCalc/StringCalc.java
+++ src/main/java/com/tddtest/StringCalc/StringCalc.java
@@ -1,4 +1,16 @@
 package com.tddtest.StringCalc;
 
 public class StringCalc {
+    int add(String numbers){
+        String[] numberArray = numbers.split(",");
+        if (numbers.length() == 0) {
+            return 0;
+        }
+
+        if (numberArray.length > 1) {
+            return Integer.parseInt(numberArray[0]) + Integer.parseInt(numberArray[1]);
+        }
+        return Integer.parseInt(numberArray[0]);
+
+    }
 }
--- src/test/java/com/tddtest/StringCalc/StringCalcTest.java
+++ src/test/java/com/tddtest/StringCalc/StringCalcTest.java
@@ -1,12 +1,37 @@
 package com.tddtest.StringCalc;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalcTest {
-
+    StringCalc calc;
     @BeforeEach
     void setUp() {
+        calc = new StringCalc();
+    }
+
+    @Test
+    void emptyString(){
+        assertEquals(0,calc.add(""));
+    }
+
+    @Test
+    void singleString(){
+        assertEquals(1,calc.add("1"));
+        assertEquals(2,calc.add("2"));
+        assertEquals(0,calc.add("0"));
+    }
+
+    @Test
+    void twoString(){
+        assertEquals(3,calc.add("1,2"));
+        assertEquals(30,calc.add("10,20"));
+    }
+
+    @Test
+    void moreThanTwoString(){
+        assertEquals(5,calc.add("2,3,4,5,6"));
     }
 }

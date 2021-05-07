43d7e2f56ff769149c3c4087f75406236949aeff
nrvjj008 <nirav@jeavio.com>
2021-05-07 11:41:38 +0530

    negative numbers exception

--- src/test/java/com/tddtest/StringCalc/StringCalcTest.java
+++ src/test/java/com/tddtest/StringCalc/StringCalcTest.java
@@ -13,39 +13,46 @@ class StringCalcTest {
     }
 
     @Test
-    void emptyString(){
+    void emptyString() throws NegativeException {
         assertEquals(0,calc.add(""));
     }
 
     @Test
-    void singleString(){
+    void singleString() throws NegativeException {
         assertEquals(1,calc.add("1"));
         assertEquals(2,calc.add("2"));
         assertEquals(0,calc.add("0"));
     }
 
     @Test
-    void twoString(){
+    void twoString() throws NegativeException {
         assertEquals(3,calc.add("1,2"));
         assertEquals(30,calc.add("10,20"));
     }
 
     @Test
-    void moreThanTwoString(){
+    void moreThanTwoString() throws NegativeException{
         assertEquals(20,calc.add("2,3,4,5,6"));
         assertEquals(10,calc.add("2,3,5"));
     }
 
     @Test
-    void newLineCharacter(){
+    void newLineCharacter() throws NegativeException{
         assertEquals(3,calc.add("1\n2"));
         assertEquals(7,calc.add("1\n2,4"));
 
     }
+
     @Test
-    void customDelimeter(){
+    void customDelimeter() throws NegativeException{
         assertEquals(6,calc.add("//;\n1;2;3"));
         assertEquals(70,calc.add("//#\n10#20#40"));
 
+    }
+
+    @Test
+    void negativeException(){
+        NegativeException negativeException = assertThrows(NegativeException.class, ()->calc.add("-1,-2,3"));
+        assertTrue(negativeException.getMessage().contains("negatives not allowed"));
     }
 }
